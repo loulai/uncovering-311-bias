38d327896a56a048960a247b3a5987257eac1e59
Louise Lai <louiselai88@gmail.com>
2017-07-25 21:18:00 -0400

    todo: summarize complaints by zip and count complaint types

--- 311.R
+++ 311.R
@@ -2,7 +2,7 @@
 d311 <- read_csv("311_service_requests_from_2011.csv")
 
 # selecting only incident zip
-d311 <-d311 %>% select(`Complaint Type`, `Incident Zip`)
+d311 <- d311 %>% select(`Complaint Type`, `Incident Zip`)
 
 # renaming
 d311 <- d311 %>% rename(complaint_type = `Complaint Type`, incident_zip = `Incident Zip`)
@@ -10,3 +10,4 @@ d311 <- d311 %>% rename(complaint_type = `Complaint Type`, incident_zip = `Incid
 # reordering
 d311 <-  d311[c(2,1)]
 
+d311 <- d311  %>% 
