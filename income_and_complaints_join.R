5885639ab25df6f0bc8f18f0a2cde7dbaec43d2e
Louise Lai <louiselai88@gmail.com>
2017-07-26 19:40:20 -0400

    trained on one variable, TODO: train on more variables

--- income_and_complaints_join.R
+++ income_and_complaints_join.R
@@ -33,14 +33,32 @@ lm.fit1 <- lm(num_total_complaints ~ mean_income, train)
 predicted_complaints <- predict(lm.fit1, test)
 
 # evaluate model on test data set
-ggplot() + geom_point(data=train, aes(mean_income, num_total_complaints), color = "gray") 
-         + geom_point(data=test, aes(mean_income, num_total_complaints, color="red"))
-         + geom_line(data=test, aes(mean_income, predicted_complaints, color = "red"))
-         + labs(color = "Test Data")
 
-ggplot() + geom_point(data=train, aes(mean_income, num_total_complaints), color = "gray") 
-         + geom_point(data=test, aes(mean_income, num_total_complaints, color = "red")) 
-         + geom_line(data=test, aes(mean_income, predicted_complaints, color = "red")) 
-         + labs(color = "Test Data")
-
+#graphing training & test data. Regression line is modeled on the training data, which is gray. It's designed to fit the test data in red.
+ggplot() + geom_point(data=train, aes(mean_income, num_total_complaints), color = "gray") + geom_point(data=test, aes(mean_income, num_total_complaints, color = "red")) + geom_line(data=test, aes(mean_income, predicted_complaints, color = "red")) + labs(color = "Test Data")
 corr <- cor(predicted_complaints, test$num_total_complaints) ^ 2
+corr # 0.036
+
+# mean_income, unemployment rate
+
+#===== fitting everything to see most significant ones
+
+#selecting all 'fair' data
+df <- select(grand, zip, mean_income, median_income, race_white, unemployment_rate, family_poverty_percent) 
+View(df)
+
+#based on unmodified data (i.e. no mutates)
+View(train)
+lm.fit2 <- lm(num_total_complaints ~ ., data = train)
+summary(lm.fit2)
+
+pred2 <- predict(lm.fit2, test)
+validation <- mutate(validation, predicted = pred1)
+View(validation)
+
+#plotting predicted vs real
+ggplot(validation, aes(predicted, total_trips)) + geom_point()
+
+#plotting regression: predicted is red, actual is grey
+ggplot(data = test) + geom_point(aes(tmin, total_trips), color = "gray") + geom_point(aes(tmin, predicted), color = "red")
+mse(lm.fit1)
