0e44a7ad360eb55d6c454140b023c8522e8afac5
Louise Lai <louiselai88@gmail.com>
2017-07-26 19:14:14 -0400

    trained model and resulted in fairly low correlation

--- income_and_complaints_join.R
+++ income_and_complaints_join.R
@@ -17,7 +17,30 @@ View(income)
 
 ###### prediction
 
-# split test/train
+# == split test/train
 index <- sample(1:nrow(grand), size = 0.2 * nrow(grand))
 
-test = 
+test = grand[index, ] #37
+train = grand[-index, ] #152
+nrow(train)
+
+# == fitting model
+
+# run regression on training data set
+lm.fit1 <- lm(num_total_complaints ~ mean_income, train)
+
+# predict values
+predicted_complaints <- predict(lm.fit1, test)
+
+# evaluate model on test data set
+ggplot() + geom_point(data=train, aes(mean_income, num_total_complaints), color = "gray") 
+         + geom_point(data=test, aes(mean_income, num_total_complaints, color="red"))
+         + geom_line(data=test, aes(mean_income, predicted_complaints, color = "red"))
+         + labs(color = "Test Data")
+
+ggplot() + geom_point(data=train, aes(mean_income, num_total_complaints), color = "gray") 
+         + geom_point(data=test, aes(mean_income, num_total_complaints, color = "red")) 
+         + geom_line(data=test, aes(mean_income, predicted_complaints, color = "red")) 
+         + labs(color = "Test Data")
+
+corr <- cor(predicted_complaints, test$num_total_complaints) ^ 2
