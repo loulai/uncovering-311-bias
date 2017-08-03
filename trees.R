459479cc98fe7bf5658c70d9d97070c8c21716b1
Louise Lai <louiselai88@gmail.com>
2017-08-03 12:44:41 -0400

    some exploration on trees

--- trees.R
+++ trees.R
@@ -22,3 +22,10 @@ trees <- trees %>% group_by(zipcode) %>% mutate(total_trees = n()) %>% distinct(
 
 
 
+###### exploration
+
+# what zip has most trees?
+# 10312 has 22,186 trees, it's in Great Kills, Staten Island. 
+
+# what zip has least trees?
+# 10115 has 7 trees, it's in Morningside Heights and is tiny! And has a population of 0. 
