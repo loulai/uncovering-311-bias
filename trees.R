365a7c4881513e317f32607ec37c34b13c5f5d7e
Louise Lai <louiselai88@gmail.com>
2017-08-08 15:02:01 -0400

    updated exploration and added line of best fit for trees analysis

--- trees.R
+++ trees.R
@@ -29,22 +29,23 @@ grand_trees <- grand_trees %>% mutate(trees_per_person = total_trees/population_
 grand_trees <- merge(tree_complaints, grand_trees, by="zip")
 
 # plotting number of trees v. number of requests for trees
-ggplot() + geom_point(data=grand_trees, aes(total_trees, new_tree_requests), color = "black") 
+ggplot(grand_trees, aes(total_trees, new_tree_requests)) + geom_point(color = "black") + geom_smooth(method='lm', se=FALSE)
 
 # plotting number of trees per person v. number of requests for trees
-ggplot() + geom_point(data=grand_trees, aes(trees_per_person, new_tree_requests), color = "black") 
+ggplot(grand_trees, aes(trees_per_person, new_tree_requests)) + geom_point(color = "black") + geom_smooth(method='lm', se=FALSE)
 
 
 ###### exploration
 
 # what zip has most trees?
-# 10312 has 22,186 trees, it's in Great Kills, Staten Island. 
+# 10312 Great Kills Staten Island has 22,186 trees
 
 # what zip has least trees?
-# 10115 has 7 trees, it's in Morningside Heights and is tiny! And has a population of 0. 
+# 10115 Morningside Heights has 7 trees (tiny! Has a population of 0).
 
 # what zip has the most trees per person?
 # 10309 Rossvile, Staten Island at 0.3811 trees per person. Every 3 people have one tree.
 
 # what zip has the least trees per person?
 # 11697 Breezy Point, NY at 0.0074. Every 135 people get one tree.
+
